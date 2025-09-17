8b1e343b93650cbf3a589cd9c36e1fabbb4f26f9
Matias Cano <matiasncano@gmail.com>
2025-09-17 14:40:17 -0300

    ajuste de cards

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,12 +5,12 @@ function Item(props) {
     <>
       <div className="item">
         <img src={props.image} alt={props.title} className="item-image" />
+        <p className="item-category">{props.category}</p>
         <a href="#">
           <h3 className="item-name">{props.title}</h3>
         </a>
-        <p className="item-category">{props.category}</p>
-        <p className="item-price">${props.price}</p>
         <p className="item-description">{props.description}</p>
+        <p className="item-price">${props.price}</p>
         <button className="item-button">Agregar al carrito</button>
       </div>
     </>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -11,49 +11,49 @@ function ItemListContainer(props) {
             title="DULCE DE LECHE BLANCO"
             category="Alfajor Blanco"
             price={3500}
-            description="Alfajor artesanal, con masa de cacao y relleno de dulce de leche . Baño cobertura blanco"
+            description="Masa de cacao y relleno de dulce de leche . Baño cobertura blanco"
             image="https://dcdn-us.mitiendanube.com/stores/004/290/481/products/blanco-y-ddl-430f92a328afb4f17e17228812908294-1024-1024.webp"
           />
           <Item
             title="CORAZÓN DE FRUTOS ROJOS BLANCO"
             category="Alfajor Blanco"
             price={3500}
-            description="Alfajor artesanal, con masa de cacao, relleno de dulce de leche y en su centro un corazón de frutos rojos. Baño cobertura blanco"
+            description="Masa de cacao, relleno de dulce de leche y en su centro un corazón de frutos rojos. Baño cobertura blanco"
             image="https://dcdn-us.mitiendanube.com/stores/004/290/481/products/corazon-blanco-736d8ce2434eae1f3e17228811083646-1024-1024.webp"
           />
           <Item
             title="DULCE DE LECHE Y NUEZ"
             category="Alfajor Blanco"
             price={3500}
-            description="Alfajor artesanal, con masa de cacao, relleno de dulce de leche y nueces. Baño cobertura blanco"
+            description="Masa de cacao, relleno de dulce de leche y nueces. Baño cobertura blanco"
             image="https://dcdn-us.mitiendanube.com/stores/004/290/481/products/nueces-bc59068f5fc283d47417228808998782-1024-1024.webp"
           />
           <Item
             title="MOUSSE DE CHOCOLATE BLANCO"
             category="Alfajor Blanco"
             price={3500}
-            description="Alfajor artesanal, con masa de cacao, relleno de mousse de chocolate blanco. Baño cobertura blanco"
+            description="Masa de cacao, relleno de mousse de chocolate blanco. Baño cobertura blanco"
             image="https://dcdn-us.mitiendanube.com/stores/004/290/481/products/mousse-blanco-4c49c475a38e13456d17228807321166-1024-1024.webp"
           />
           <Item
             title="CAFÉ AL COÑAC"
             category="Alfajor Negro"
             price={3500}
-            description="Alfajor artesanal, con masa de cacao, relleno con mousse de café al coñac Baño cobertura negro. "
+            description="Masa de cacao, relleno con mousse de café al coñac Baño cobertura negro. "
             image="https://dcdn-us.mitiendanube.com/stores/004/290/481/products/cafe-al-conac-50d2684414b97d192117228799404533-1024-1024.webp"
           />
           <Item
             title="MOUSSE CON CORAZÓN DE NARANJA"
             category="Alfajor Negro"
             price={3500}
-            description="Alfajor artesanal, con masa de cacao, relleno de mousse de chocolate negro y en su centro un corazón de naranja. Baño cobertura negro."
+            description="Masa de cacao, relleno de mousse de chocolate negro y en su centro un corazón de naranja. Baño cobertura negro."
             image="https://dcdn-us.mitiendanube.com/stores/004/290/481/products/naranja-y-mousse-f0129c786a9b16ff1217228803391165-1024-1024.webp"
           />
           <Item
             title="MOUSSE DE CHOCOLATE"
             category="Alfajor Negro"
             price={3500}
-            description="Alfajor artesanal, con masa de cacao, relleno mousse de chocolate negro. Baño cobertura negro."
+            description="Masa de cacao, relleno mousse de chocolate negro. Baño cobertura negro."
             image="https://dcdn-us.mitiendanube.com/stores/004/290/481/products/mousse-negro-af97da2942e0fbeabc17228805618120-1024-1024.webp"
           />
         </div>
