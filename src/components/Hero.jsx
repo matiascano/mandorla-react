9cacc7ceb08b6eec49100c4bdf6be029ec4a8812
Matias Cano <matiasncano@gmail.com>
2025-10-09 12:06:26 -0300

    ajustes visuales al hero para que quede del mismo ancho que el contenido

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,14 @@ function Hero() {
   return (
     <>
       <section className="hero">
-        <div className="hero-content">
-          <h1>Alfajores artesanales gourmet</h1>
-          <p>¡Hacete un mimo y date un gustito!</p>
-        </div>
-        <div className="hero-image">
-          <img src="/img/hero.avif" alt="Hero" />
+        <div className="hero-container">
+          <div className="hero-content">
+            <h1>Alfajores artesanales gourmet</h1>
+            <p>¡Hacete un mimo y date un gustito!</p>
+          </div>
+          <div className="hero-image">
+            <img src="/img/hero.avif" alt="Hero" />
+          </div>
         </div>
       </section>
     </>
