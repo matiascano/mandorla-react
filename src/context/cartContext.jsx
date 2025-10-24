99a5ecaf8a6a5ff93a7f653f41e1aa22d86425d7
Matias Cano <matiasncano@gmail.com>
2025-10-24 13:54:54 -0300

    se ajustó el add para que no duplique el producto si ya existe sino que lo sume

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -6,7 +6,19 @@ export function CartContextProvider({ children }) {
   const [cart, setCart] = useState([]);
 
   function addToCart(item) {
-    setCart((prevCart) => [...prevCart, item]);
+    // Agrega un artículo al carrito si ya está en el carrito, actualiza la cantidad
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((cartItem) => cartItem.id === item.id);
+      if (existingItem) {
+        return prevCart.map((cartItem) =>
+          cartItem.id === item.id
+            ? { ...cartItem, quantity: cartItem.quantity + item.quantity }
+            : cartItem
+        );
+      } else {
+        return [...prevCart, item];
+      }
+    });
   }
 
   function removeFromCart(itemId) {
